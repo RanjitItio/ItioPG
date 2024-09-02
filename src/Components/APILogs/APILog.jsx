57e254907c6c408c133664b36a503a93ef9a6d6c
Sandeep <sandeeps@bigit.io>
2024-09-02 17:58:58 +0530

    API Logs

--- src/Components/APILogs/APILog.jsx
+++ src/Components/APILogs/APILog.jsx
@@ -1,80 +1,140 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Pagination } from 'antd';
+import { useEffect, useState } from 'react';
+import axiosInstance from '../Authentication/axios';
 
 
 
 const columns = [
   {
     title: (
-        <div style={{ textAlign: 'center'}}>
+        <div style={{ textAlign: 'center', fontWeight: 'bold', backgroundColor: '#f5f5f5', padding: '10px' }}>
             APIs
         </div>
     ),
-    dataIndex: 'apis',
+    dataIndex: 'url',
     key: 'apis',
     width: 150,
     fixed: 'left',
   },
   {
     title: (
-        <div style={{ textAlign: 'center'}}>
+        <div style={{ textAlign: 'center', fontWeight: 'bold', backgroundColor: '#f5f5f5', padding: '10px' }}>
             Errors
         </div>
     ),
-    dataIndex: 'errors',
+    dataIndex: 'error',
     key: 'errors',
     fixed: 'left',
-    width: 150,  
+    width: 150,
+    render: (text)=> <span style={{ color: '#c34a36', fontWeight: 'bold' }}>{text}</span>
   },
   {
-    title: 'Logs',
+    title: (
+      <div style={{ textAlign: 'center', fontWeight: 'bold', backgroundColor: '#f5f5f5', padding: '10px' }}>
+          Logs
+      </div>
+    ),
     fixed: 'left',
     key: 'logs',
     children: [
       {
         title: 'Request Header',
-        dataIndex: 'requestHeader',
+        dataIndex: 'request_header',
         key: 'requestHeader',
     
       },
       {
         title: 'Request Body',
-        dataIndex: 'requestBody',
+        dataIndex: 'request_body',
         key: 'requestBody',
+        render: (text) => <span style={{ color:'#008ac3' }}>{text}</span>
       },
       {
         title: 'Response Header',
-        dataIndex: 'responseHeader',
+        dataIndex: 'response_header',
         key: 'responseHeader',
       },
       {
         title: 'Response Body',
-        dataIndex: 'responseBody',
+        dataIndex: 'response_body',
         key: 'responseBody',
+        render: (text) => <span style={{ wordBreak: 'break-word' }}>{JSON.stringify(text)}</span>
       },
     ],
   },
 ];
 
 
-const data = [];
 
 
 
-const APILogs = () => (
-    <Table
-      columns={columns}
-      dataSource={data}
-      bordered
-      size="middle"
-      scroll={{
-        x: 1500,
-        y: 240,
-      }}
-    />
-  
-);
+// Merchant Transaction Logs
+export default function APILogs() {
+  const [logData, updateLogData] = useState([]);
+  const [totalRowCount, setTotalRowCount] = useState();
+
+
+  // Fetch all the Merchant logs
+  useEffect(() => {
+      axiosInstance.get(`api/v5/merchant/transaction/logs/`).then((res)=> {
+        // console.log(res)
+
+        if (res.status === 200 && res.data.success === true) {
+          updateLogData(res.data.merchant_logs.map((log, index) => ({...log, key: index})))
+          setTotalRowCount(res.data.total_rows)
+        };
+
+      }).catch((error)=> {
+        console.log(error)
+      })
+  }, []);
+
+
+   // Get paginatede Data
+  const handlePaginationValue = (page, pageSize)=> {
+    // console.log(page)
+      let limit = 10;
+      let offset = (page - 1) * limit;
+      
+      axiosInstance.get(`api/v5/merchant/transaction/logs/?limit=${limit}&offset=${offset}`).then((res)=> {
+        // console.log(res)
+
+        if (res.status === 200 && res.data.success === true) {
+          updateLogData(res.data.merchant_logs.map((log, index) => ({...log, key: index})))
+        }
+
+      }).catch((error)=> {
+        console.log(error);
+
+      })
+  };
 
 
 
-export default APILogs;
+   return (
+    <>
+        <Table
+          columns={columns}
+          dataSource={logData}
+          pagination={false}
+          bordered
+          size="middle"
+          scroll={{
+            x: 1500,
+            y: 380,
+          }}
+          rowClassName="custom-row"
+        />
+
+        <div style={{ marginTop: '1px', textAlign: 'right' }}>
+            <Pagination 
+              defaultCurrent={1} 
+              total={totalRowCount} 
+              onChange={handlePaginationValue}
+              />
+        </div>
+    </>
+   );
+};
+
